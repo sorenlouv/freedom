f05d97744a56c1f0c9695380221de2446e7a1aec
Søren Louv <sorenlouv@gmail.com>
2013-03-12 18:53:00 +0100

    Backend Analytics tracking

--- feed.php
+++ feed.php
@@ -4,6 +4,7 @@ header('Content-Disposition: attachment; filename="feed.ics"');
 
 // header('Content-Type: text/plain; charset=utf-8');
 
+include_once("analyticstracking.php");
 require_once 'ical.php';
 $ical = new ical($_GET["uid"], $_GET["key"]);
 echo $ical->get_filtered_content();
