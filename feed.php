ff34683fcf63404b18f4894d9345e3e56a062f65
Søren Louv <sorenlouv@gmail.com>
2013-03-13 04:07:02 +0100

    Added Facebook SDK

--- feed.php
+++ feed.php
@@ -1,10 +1,11 @@
 <?php
-header('Content-type: text/calendar;charset=utf-8');
-header('Content-Disposition: attachment; filename="feed.ics"');
+// header('Content-type: text/calendar;charset=utf-8');
+// header('Content-Disposition: attachment; filename="feed.ics"');
 
-// header('Content-Type: text/plain; charset=utf-8');
+header('Content-Type: text/plain; charset=utf-8');
 
+require_once "facebook-sdk/facebook.php";
 require_once 'ical.php';
-$ical = new ical($_GET["uid"], $_GET["key"]);
-echo $ical->get_filtered_content();
+$ical = new ical($_GET["uid"], $_GET["key"], $_GET["access_token"]);
+echo $ical->get_feed();
 ?>
