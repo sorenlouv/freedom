fb609c952dd4d4c2e6115078f6915f3bbc9afc69
Søren Louv <sorenlouv@gmail.com>
2013-03-14 20:54:30 +0100

    Wrapped in query ready

--- js/global.js
+++ js/global.js
@@ -1,81 +1,84 @@
-// submit feed
-$('#facebook-feed').submit(function(e){
-    e.preventDefault();
+$(document).ready(function(){
 
-    // hide all alerts
-    $('.alert').hide();
+    // submit feed
+    $('#facebook-feed').submit(function(e){
+        e.preventDefault();
 
-    var originalFeed = $(this).children('input')[0].value;
-    var queryString = originalFeed.match(/uid=(\d+)&key=(\w+)/);
+        // hide all alerts
+        $('.alert').hide();
 
-    // not valid
-    if(queryString === null){
-        $(".alert-error").text('Error: the URL was not valid').fadeIn();
+        var originalFeed = $(this).children('input')[0].value;
+        var queryString = originalFeed.match(/uid=(\d+)&key=(\w+)/);
 
-        // Add error to Google Analytics
-        _gaq.push(['_trackEvent', 'feedSubmitted', 'error', originalFeed]);
+        // not valid
+        if(queryString === null){
+            $(".alert-error").text('Error: the URL was not valid').fadeIn();
+
+            // Add error to Google Analytics
+            _gaq.push(['_trackEvent', 'feedSubmitted', 'error', originalFeed]);
+
+            return false;
+        }
+
+        var uid = queryString[1];
+        var key = queryString[2];
+
+        var newWebcal = "webcal://freedom.pagodabox.com/feed.ics?uid=" + uid + "&key=" + key;
+        var googleLink = "http://www.google.com/calendar/render?cid=" + encodeURIComponent(newWebcal);
+
+        // Add succes event to GA
+        _gaq.push(['_trackEvent', 'feedSubmitted', 'success', uid]);
+
+        // Update links
+        $("a.import-feed").attr('href', googleLink);
+        $("a.download-feed").attr('href', newWebcal);
+
+        // appear
+        $(".alert-success").fadeIn();
 
         return false;
-    }
+    });
 
-    var uid = queryString[1];
-    var key = queryString[2];
+    // click signin with facebook
+    $('.facebook-connect-button').click(function(e){
+        e.preventDefault();
 
-    var newWebcal = "webcal://freedom.pagodabox.com/feed.ics?uid=" + uid + "&key=" + key;
-    var googleLink = "http://www.google.com/calendar/render?cid=" + encodeURIComponent(newWebcal);
+        // hide all alerts
+        $('.alert').hide();
 
-    // Add succes event to GA
-    _gaq.push(['_trackEvent', 'feedSubmitted', 'success', uid]);
+        var spinner = new Spinner().spin(document.getElementById('clear-div'));
 
-    // Update links
-    $("a.import-feed").attr('href', googleLink);
-    $("a.download-feed").attr('href', newWebcal);
+        FB.login(function(response) {
+            if (response.authResponse) {
 
-    // appear
-    $(".alert-success").fadeIn();
+                // short lived access token
+                var accessTokenShort = FB.getAuthResponse()['accessToken'];
 
-    return false;
-});
+                // extend access token
+                $.getJSON('/extend_token.php?access_token=' + accessTokenShort, function(response) {
+                    var accessTokenLong = response.access_token;
 
-// click signin with facebook
-$('.facebook-connect-button').click(function(e){
-    e.preventDefault();
+                    var newWebcal = "webcal://freedom.pagodabox.com/feed.ics?access_token=" + accessTokenLong;
+                    var googleLink = "http://www.google.com/calendar/render?cid=" + encodeURIComponent(newWebcal);
 
-    // hide all alerts
-    $('.alert').hide();
+                    // Add success event to GA
+                    _gaq.push(['_trackEvent', 'feedSubmitted', 'success', 'facebook']);
 
-    var spinner = new Spinner().spin(document.getElementById('clear-div'));
+                    // Update links
+                    $("a.import-feed").attr('href', googleLink);
+                    $("a.download-feed").attr('href', newWebcal);
 
-    FB.login(function(response) {
-        if (response.authResponse) {
+                    // appear
+                    $(".alert-success").fadeIn();
 
-            // short lived access token
-            var accessTokenShort = FB.getAuthResponse()['accessToken'];
+                    //stop spinner
+                    spinner.stop();
 
-            // extend access token
-            $.getJSON('/extend_token.php?access_token=' + accessTokenShort, function(response) {
-                var accessTokenLong = response.access_token;
+                });
 
-                var newWebcal = "webcal://freedom.pagodabox.com/feed.ics?access_token=" + accessTokenLong;
-                var googleLink = "http://www.google.com/calendar/render?cid=" + encodeURIComponent(newWebcal);
-
-                // Add success event to GA
-                _gaq.push(['_trackEvent', 'feedSubmitted', 'success', 'facebook']);
-
-                // Update links
-                $("a.import-feed").attr('href', googleLink);
-                $("a.download-feed").attr('href', newWebcal);
-
-                // appear
-                $(".alert-success").fadeIn();
-
-                //stop spinner
-                spinner.stop();
-
-            });
-
-        }else{
-            $(".alert-error").text('Facebook connect failed').fadeIn();
-        }
-    }, {scope: 'offline_access, user_events'});
+            }else{
+                $(".alert-error").text('Facebook connect failed').fadeIn();
+            }
+        }, {scope: 'offline_access, user_events'});
+    });
 });
