9b7109c64001f50bd375f12ce14ba59e04649156
Søren Louv <sorenlouv@gmail.com>
2013-03-23 18:32:17 +0100

    Damn… So many change in one commit!
    Added compass
    Added Server side analytics
    Simplified setup and removed standard Facebook iCal feed
    Added Step Wizard (™)

---
+++ handlers.php
@@ -0,0 +1,55 @@
+<?php
+
+// Disclaimer: fuckly handling of URLs to methods, because I don't wanna setup complete routing framework
+// I know I've sinned...
+
+if(function_exists($_GET['f'])) {
+   $_GET['f']();
+}else{
+    header('HTTP/1.0 501 Not Found');
+}
+
+// extend facebook access token
+function saveAccessToken(){
+    // includes
+    include("utils.php");
+    $facebook = Utils::get_facebook_object();
+
+    // arguments
+    $access_token_short = $facebook->getAccessToken();
+    $facebook_id = $facebook->getUser();
+
+    // headers
+    header("Content-type: application/json");
+
+    // extend access token
+    $access_token = Utils::extend_access_token($facebook, $access_token_short);
+
+    // secure hash
+    $secure_hash = Utils::get_secure_hash($facebook_id);
+
+    // save access token
+    Utils::save_access_token($facebook_id, $access_token, $secure_hash);
+
+    // output
+    echo json_encode(array("secure_hash" => $secure_hash));
+}
+
+// download or fetch Facebook feed
+function downloadFeed(){
+    // includes
+    require_once 'Feed.php';
+
+    // headers
+    header('Content-type: text/calendar;charset=utf-8');
+    header('Content-Disposition: attachment; filename=feed.ics');
+
+    // arguments
+    $secure_hash = isset($_GET["secure_hash"]) ? $_GET["secure_hash"] : null;
+    $user_id = isset($_GET["user_id"]) ? $_GET["user_id"] : null;
+    $access_token = isset($_GET["access_token"]) ? $_GET["access_token"] : null;
+
+    // output
+    $Feed = new Feed($user_id, $secure_hash, $access_token);
+    echo $Feed->get_feed();
+}
--- pagoda/config.php
+++ pagoda/config.php
@@ -1,4 +1,12 @@
 <?php
+    // Facebook
     $CLIENT_ID = $_SERVER["CLIENT_ID"];
     $CLIENT_SECRET = $_SERVER["CLIENT_SECRET"];
+    $SALT = $_SERVER["SALT"];
+
+    // MYSQL
+    $DB_NAME = $_SERVER["DB1_NAME"];
+    $DB_USER = $_SERVER["DB1_USER"];
+    $DB_PASSWORD = $_SERVER["DB1_PASSWORD"];
+    $DB_HOST = $_SERVER["DB1_HOST"];
 ?>
