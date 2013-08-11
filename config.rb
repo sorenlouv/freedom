b87f3dfd72bcee7fdde896beb5cb0f808f6cf9c1
Søren Louv <sorenlouv@gmail.com>
2013-08-11 13:53:10 +0200

    Reinstalled Foundation (goodbye bootstrap)

--- config.rb
+++ config.rb
@@ -1,15 +1,16 @@
+require 'zurb-foundation'
 # Require any additional compass plugins here.
+
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
+css_dir = "stylesheets"
 sass_dir = "sass"
-images_dir = "img"
-javascripts_dir = "js"
+images_dir = "images"
+javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
