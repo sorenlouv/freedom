9ed97cd9063d8b24eb4be4b4d55813aee72745ef
Søren Louv-Jansen <sqren@tradeshift.com>
2013-09-05 19:38:05 +0200

    Test calendar

---
+++ test-cal.php
@@ -0,0 +1,410 @@
+<?php
+    header('Content-type: text/calendar;charset=utf-8');
+    header('Content-Disposition: attachment; filename=feed.ics');
+?>
+BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//Facebook//NONSGML Facebook Events V1.0//EN
+X-WR-CALNAME:FB Freedom
+X-PUBLISHED-TTL:PT12H
+X-ORIGINAL-URL:https://www.facebook.com/events/
+CALSCALE:GREGORIAN
+METHOD:PUBLISH
+BEGIN:VEVENT
+DTSTAMP:20130806T104626Z
+LAST-MODIFIED:20130806T104626Z
+CREATED:20130806T104626Z
+SEQUENCE:0
+ORGANIZER;CN=Radek Wiglasz:MAILTO:noreply@facebookmail.com
+DTSTART;VALUE=DATE-TIME:20140717T120000Z
+DTEND;VALUE=DATE-TIME:20140720T213000Z
+UID:e698369596843095@facebook.com
+SUMMARY:Colours 2014
+LOCATION:Dolni Oblast Vitkovice
+URL:http://www.facebook.com/events/698369596843095/
+DESCRIPTION:Go to
+ event:\nhttp://www.facebook.com/events/698369596843095
+CLASS:PUBLIC
+STATUS:CONFIRMED
+PARTSTAT:ACCEPTED
+END:VEVENT
+BEGIN:VEVENT
+DTSTAMP:20130817T224708Z
+LAST-MODIFIED:20130817T224708Z
+CREATED:20130817T224708Z
+SEQUENCE:0
+ORGANIZER;CN=Zuzana Pelánková:MAILTO:noreply@facebookmail.com
+DTSTART;VALUE=DATE-TIME:20131018T153000Z
+DTEND;VALUE=DATE-TIME:20131020T160000Z
+UID:e161400140718368@facebook.com
+SUMMARY:Punkový Orličky
+LOCATION:Orlicky 78\, Pardubicky Kraj
+URL:http://www.facebook.com/events/161400140718368/
+DESCRIPTION:Punk's not dead! Oslava mých narozenin se
+ uskuteční tradičně na chalupě v Orličkách\,
+ netradičně až v říjnu\, dva měsíce po mých
+ narozkách\, ale to neva. Letošní ročník bude
+ v duchu punku. Zvu vás s dostatečným
+ předstihem\, abyste stihli sehnat křiváky\,
+ stahováky a  načesat číra.A natrénovat pogo.
+ No idols! No future! Fuck system! \n\nGo to
+ event:\nhttp://www.facebook.com/events/161400140718368
+CLASS:PUBLIC
+STATUS:CONFIRMED
+PARTSTAT:ACCEPTED
+END:VEVENT
+BEGIN:VEVENT
+DTSTAMP:20130627T192447Z
+LAST-MODIFIED:20130627T192447Z
+CREATED:20130627T192447Z
+SEQUENCE:0
+ORGANIZER;CN=Markéta Čejková:MAILTO:noreply@facebookmail.com
+DTSTART;VALUE=DATE-TIME:20131006T153000Z
+DTEND;VALUE=DATE-TIME:20131006T183000Z
+UID:e390969071006962@facebook.com
+SUMMARY:Za kulturou tentokrát s Markétou
+LOCATION:Divadlo Bolka Polivky
+URL:http://www.facebook.com/events/390969071006962/
+DESCRIPTION:Pro velký úspěch kulturní akce s Bakinem je tu
+ další událost! Pro všechny\, koho v dětství
+ (ale i v dospělosti) bavily Mikulášovy
+ patálie\, je tu jedinečná událost v podání
+ divadelního spolku Kašpar z "Prahe". Inscenace
+ je natolik úspěšná\, že se letos o
+ prázdinách dočká pokračování - Mikulášovy
+ přázdniny. Ale na to půjdem až někdy
+ příště...\nJinak - může se zdát\, že je to
+ až za dlouho\, ale z vlastní zkušenosti mám
+ pocit\, že v divadle bolka polívky bývá
+ vždycky vyprodáno dlouho dopředu. Takže
+ neváhejte!
+ :-)\n\nhttp://www.divadlovceletne.cz/program/mikulasovy-patalie/\nhttp://www.divadlobolkapolivky.cz/program/blog.html\n\nA
+ ještě dvě náhodně vybrané
+ recencze:\n\nhttp://kultura.idnes.cz/recenze-nove-patalie-by-si-nemely-deti-nechat-ujit-ani-jejich-rodice-1fr-/divadlo.aspx?c=A120130_164720_divadlo_jaz\n\nhttp://www.rozhlas.cz/mozaika/divadlo/_zprava/mikulasovy-patalie-recenze--1008191\n\n\nGo
+ to
+ event:\nhttp://www.facebook.com/events/390969071006962
+CLASS:PUBLIC
+STATUS:CONFIRMED
+PARTSTAT:ACCEPTED
+END:VEVENT
+BEGIN:VEVENT
+DTSTAMP:20130828T111803Z
+LAST-MODIFIED:20130828T111803Z
+CREATED:20130828T111803Z
+SEQUENCE:0
+ORGANIZER;CN=Zuzana Válková:MAILTO:noreply@facebookmail.com
+DTSTART;VALUE=DATE-TIME:20131006T140000Z
+DTEND;VALUE=DATE-TIME:20131006T170000Z
+UID:e703459633003573@facebook.com
+SUMMARY:Žít dechovčičku
+LOCATION:Semilasso
+URL:http://www.facebook.com/events/703459633003573/
+DESCRIPTION:Vážené dámičky a vážení džentlmeni\,
+ \nnenechme si ujít tuto mimořádnou
+ příležitost a pojďme po delší době
+ společně žít dechovku!!!1 \nAvizuju snad
+ dostatečně dopředu\, tak běda jak dáte košem
+ naší oblíbené Královopolce. :)\n\nGo to
+ event:\nhttp://www.facebook.com/events/703459633003573
+CLASS:PUBLIC
+STATUS:CONFIRMED
+PARTSTAT:ACCEPTED
+END:VEVENT
+BEGIN:VEVENT
+DTSTAMP:20130830T064657Z
+LAST-MODIFIED:20130830T064657Z
+CREATED:20130830T064657Z
+SEQUENCE:0
+ORGANIZER;CN=Petr Viktorin:MAILTO:noreply@facebookmail.com
+DTSTART;VALUE=DATE:20130922
+DTEND;VALUE=DATE:20130923
+UID:e316558888489555@facebook.com
+SUMMARY:Žít Kino sprint
+URL:http://www.facebook.com/events/316558888489555/
+DESCRIPTION:Pojďte na sprint! další možnost se potkat a
+ sdílet know-how\, jen trochu víc "hands-on" než
+ přednášky na sraze\, a s trochu
+ konkrétnějším dopadem na nějaký milý
+ open-source projekt. \n\nTentokrát Honzovo
+ zitkino.cz
+ (https://github.com/honzajavorek/zitkino.cz/)!\n\nGo
+ to
+ event:\nhttp://www.facebook.com/events/316558888489555
+CLASS:PUBLIC
+STATUS:CONFIRMED
+PARTSTAT:ACCEPTED
+END:VEVENT
+BEGIN:VEVENT
+DTSTAMP:20130818T201505Z
+LAST-MODIFIED:20130818T201505Z
+CREATED:20130818T201505Z
+SEQUENCE:0
+ORGANIZER;CN=Zuzana Pelánková:MAILTO:noreply@facebookmail.com
+DTSTART;VALUE=DATE-TIME:20130921T073000Z
+DTEND;VALUE=DATE-TIME:20130921T103000Z
+UID:e557615780970715@facebook.com
+SUMMARY:Brněnské kolečko
+LOCATION:kolem Brna
+URL:http://www.facebook.com/events/557615780970715/
+DESCRIPTION:Před dvěma lety jsme založili fajn sportovní
+ tradici\, takže ani letos nesmíme vynechat! Na
+ podzim opět obkroužíme Brno na kole po
+ cyklotrase č. 5005. Sraz je v sobotu 21.9. v 9:30
+ na konečné jedničky v Řečkovicích. Pojedeme
+ opět po směru hodinových ručiček\, takže se
+ můžete připojovat po cestě\, jak je
+ libo.\n\nGo to
+ event:\nhttp://www.facebook.com/events/557615780970715
+CLASS:PUBLIC
+STATUS:CONFIRMED
+PARTSTAT:ACCEPTED
+END:VEVENT
+BEGIN:VEVENT
+DTSTAMP:20130903T203126Z
+LAST-MODIFIED:20130903T203126Z
+CREATED:20130903T203126Z
+SEQUENCE:0
+ORGANIZER;CN=Michal Švec:MAILTO:noreply@facebookmail.com
+DTSTART;VALUE=DATE-TIME:20130906T170000Z
+DTEND;VALUE=DATE-TIME:20130906T200000Z
+UID:e441094302672883@facebook.com
+SUMMARY:Misa ma narozeniny a ma prani jediny - PARTY!!!
+LOCATION:Brno
+URL:http://www.facebook.com/events/441094302672883/
+DESCRIPTION:Koncim s mladistvym vekem\, tak bychom to meli
+ oslavit!!! \n\nNavrhuju zacit u me\, dat nejakou
+ hru\, nejaky to pejvo a pak vyrazit na Fledu na
+ Balkanskou
+ party!!\n\nhttps://www.facebook.com/events/210197085805022/\n\nZvete
+ koho chcete\, nebo rikejte na koho jsem zapomnel\,
+ navrhujte jiny mista\, jinej program\, cokoliv...
+ Jen pojdme nekam v patek a zapijme to!!\n\n\nGo to
+ event:\nhttp://www.facebook.com/events/441094302672883
+CLASS:PUBLIC
+STATUS:CONFIRMED
+PARTSTAT:ACCEPTED
+END:VEVENT
+BEGIN:VEVENT
+DTSTAMP:20130902T124914Z
+LAST-MODIFIED:20130902T124914Z
+CREATED:20130902T124914Z
+SEQUENCE:0
+ORGANIZER;CN=Turistické informační centrum města Brna, příspěvková organizace - TIC Brno:MAILTO:noreply@facebookmail.com
+DTSTART;VALUE=DATE-TIME:20130903T180000Z
+DTEND;VALUE=DATE-TIME:20130903T210000Z
+UID:e649575928394020@facebook.com
+SUMMARY:MILOKRAJ - Marta Töpferová a Tomáš Liška
+LOCATION:Pasáž Dominik\, Dominikánská ulice 15-19\,
+ Brno
+URL:http://www.facebook.com/events/649575928394020/
+DESCRIPTION:Marta Töpferová - zpěv\, Stano Palúch -
+ housle\, Marcel Comendant - cimbál\, David
+ Dorůžka - kytara a Tomáš Liška -
+ kontrabas\n\nNová hudba čerpající z
+ českých\, moravských a dalších slovanských
+ tradic Marta Töpferová a Tomáš Liška\, dva
+ vizionářští čeští hudebníci\, spolu
+ vystupují přes pět let. Po mnohých turné a
+ inspirujících hudebních výměnách\,
+ Töpferová a Liška toužili vytvořit projekt\,
+ který by čerpal z hudby jejich kořenů. Z této
+ dynamické spolupráce se zrodila sbírka
+ oslňujících autorských písní čerpajíc z
+ českého a moravského folklóru a dalších
+ slovanských
+ vlivů.\n\nhttp://www.milokraj.com/index.php/cz/\n\nGo
+ to
+ event:\nhttp://www.facebook.com/events/649575928394020
+CLASS:PUBLIC
+STATUS:CONFIRMED
+PARTSTAT:ACCEPTED
+END:VEVENT
+BEGIN:VEVENT
+DTSTAMP:20130830T112826Z
+LAST-MODIFIED:20130830T112826Z
+CREATED:20130830T112826Z
+SEQUENCE:0
+ORGANIZER;CN=Knihovna Na Křižovatce:MAILTO:noreply@facebookmail.com
+DTSTART;VALUE=DATE-TIME:20130903T150000Z
+DTEND;VALUE=DATE-TIME:20130903T180000Z
+UID:e574593112608397@facebook.com
+SUMMARY:Herden
+LOCATION:Knihovna Na Křižovatce
+URL:http://www.facebook.com/events/574593112608397/
+DESCRIPTION:Přijďte strávit příjemné odpoledne s
+ deskovými hrami do knihovny. Herden je určen pro
+ všechny\, kteří si rádi hrají a nezáleží
+ na tom\, jestli je vám 8 nebo 80 let. K dispozici
+ budou vaše oblíbené hry - vyberte podle
+ nálady\, co se bude hrát! :) Můžete si donést
+ i svoji hru.\n\nGo to
+ event:\nhttp://www.facebook.com/events/574593112608397
+CLASS:PUBLIC
+STATUS:CONFIRMED
+PARTSTAT:ACCEPTED
+END:VEVENT
+BEGIN:VEVENT
+DTSTAMP:20130812T190735Z
+LAST-MODIFIED:20130812T190735Z
+CREATED:20130812T190735Z
+SEQUENCE:0
+ORGANIZER;CN=Marek Timko:MAILTO:noreply@facebookmail.com
+DTSTART;VALUE=DATE-TIME:20130830T180000Z
+DTEND;VALUE=DATE-TIME:20130830T210000Z
+UID:e160468530808603@facebook.com
+SUMMARY:XVIII. literární večer na téma: ostré
+ záblesky z hlubin temného středověku!
+LOCATION:Ottoman Trumpet
+URL:http://www.facebook.com/events/160468530808603/
+DESCRIPTION:Drazí přátelé a literární nadšenci\,
+ srdečně vás všechny zveme na další z
+ našich/vašich oblíbených
+ literárně-hudebních čtení\, tentokrát už
+ druhé speciálně "prázdninové čtení" v
+ (ještě pořád) "novém" prostoru. \n\nMůžete
+ se těšit na to nejstrašnější a
+ nejtemnější ze středověké literatury.
+ Chudoba & hlad\, nemoci & černá smrt\, mučení
+ kačířů a upalování čarodějnic! Ale také
+ bezbťehá láska (nejenom k Bohu!)\, smyslnost a
+ rytířské (či řitířské?)
+ ctnosti...\n\nOsobní i tvůrčí iniciativa v
+ autorském bloku (tedy i mimo téma) opět a
+ pořád vítána!\n\nHudebně bude už klasicky
+ doprovázet kulturní úderka
+ Broug.\nhttps://www.facebook.com/tnbroug?ref=ts&fref=ts\n\nGo
+ to
+ event:\nhttp://www.facebook.com/events/160468530808603
+CLASS:PUBLIC
+STATUS:CONFIRMED
+PARTSTAT:ACCEPTED
+END:VEVENT
+BEGIN:VEVENT
+DTSTAMP:20130807T184947Z
+LAST-MODIFIED:20130807T184947Z
+CREATED:20130807T184947Z
+SEQUENCE:0
+ORGANIZER;CN=Martin Bakeš:MAILTO:noreply@facebookmail.com
+DTSTART;VALUE=DATE:20130830
+DTEND;VALUE=DATE:20130831
+UID:e572371949493824@facebook.com
+SUMMARY:On The Road festival aneb s bakinem za kulturou do
+ Východních Čech
+LOCATION:Lazne Bohdanec\, Pardubice
+URL:http://www.facebook.com/events/572371949493824/
+DESCRIPTION:I cesta může být cíl\, tak společně vyražme
+ na poslední prázninovou cestu a buďme On The
+ Road.\n\nOn the Road je malej festival v Lázních
+ Bohdaneč (Pardubice)\, na kterej jsem jezdil
+ ještě kdysi dávno s lidma z gymplu. Mám chuť
+ tam letos jet\, ale lidi z gymplu už ne. Tak bych
+ měl kolem sebe rád lidi\, se kterejma bych si to
+ tam užil. Doufám\, že mě v tom nenecháte.
+ \n\nFestival se koná poslední prázdninový
+ pátek a sobotu v přírodním areálu kousek za
+ Bohdančí. Spaní ve stanovém městečku
+ (zdarma) hned vedle areálu. Cena dvoudenního
+ lístku v předprodeji je krásných 250
+ Kč.\n\nPozval jsem zatim jen pár lidí\, se
+ kterejma už jsem se o tom povětšinou bavil\,
+ ale klidně zvěte i další.\n\ninfo o
+ festivalu:\nhttp://www.ontheroadfestival.com/\nhttps://www.facebook.com/ontheroadfestival\n\n\n\nGo
+ to
+ event:\nhttp://www.facebook.com/events/572371949493824
+CLASS:PUBLIC
+STATUS:CONFIRMED
+PARTSTAT:ACCEPTED
+END:VEVENT
+BEGIN:VEVENT
+DTSTAMP:20130822T111014Z
+LAST-MODIFIED:20130822T111014Z
+CREATED:20130822T111014Z
+SEQUENCE:0
+ORGANIZER;CN=Petr Viktorin:MAILTO:noreply@facebookmail.com
+DTSTART;VALUE=DATE-TIME:20130829T170000Z
+DTEND;VALUE=DATE-TIME:20130829T200000Z
+UID:e366247196812147@facebook.com
+SUMMARY:Brněnské PyVo + BRUG: Testovací
+LOCATION:U Dřeváka
+URL:http://www.facebook.com/events/366247196812147/
+DESCRIPTION:Funguje to\, co jste napsali? Jste si jistí?
+ Pojďme si popovídat o tom\, jak kontrolovat\,
+ jestli jsme ňekde neudělali
+ chybu.\n\nhttp://lanyrd.com/2013/brnenske-pyvo-brug-srpnove/\n\nGo
+ to
+ event:\nhttp://www.facebook.com/events/366247196812147
+CLASS:PUBLIC
+STATUS:CONFIRMED
+PARTSTAT:ACCEPTED
+END:VEVENT
+BEGIN:VEVENT
+DTSTAMP:20130823T090823Z
+LAST-MODIFIED:20130823T090823Z
+CREATED:20130823T090823Z
+SEQUENCE:0
+ORGANIZER;CN=Fléda Club:MAILTO:noreply@facebookmail.com
+DTSTART;VALUE=DATE-TIME:20130823T190000Z
+DTEND;VALUE=DATE-TIME:20130823T220000Z
+UID:e347850302011944@facebook.com
+SUMMARY:LETNÍ BALKAN BASHAVEL @ FLÉDA
+LOCATION:Fléda Club
+URL:http://www.facebook.com/events/347850302011944/
+DESCRIPTION:LETNÍ VYDÁNÍ TANČÍRNY BALKAN BASHAVEL
+ \n\nMilujeme temperamentní beaty Balkánu\,
+ živelnost cikánské muziky\, kolumbíjské
+ cumbie a pařížského electro swingu!\n\nDo Brna
+ domácí MALALATA přiveze jednoho z
+ nejpovolanějších „balkanizátorů“ na
+ československé scéně! Společně s MALALATA
+ tradičně vystoupí taky trumpetista\, dobrý
+ kamarád a člen klezmer kapely - Pohřební
+ kapela\, Pepíno.
+ \n\n●●●●●●●●●●●●●●●●●●●●●●●●●\n\nMALALATA
+ SOUND SYSTEM (SK) & live trumpet\n\nOdehrála
+ vystopení s kapelami a producenty jako Parov
+ Stelar Band\, N.O.H.A\, Fanfare Ciocarlia\, Mahala
+ Rai Banda\, Nouvelle Vague\, Nneka\, Watcha Clan\,
+ Parno Graszt na bujarých večírcích v Praze\,
+ Bratislavě Varšavě\, Berlíně\, Vídni či
+ Budapešti.\n\nwww.malalata.sk\nwww.facebook.com/malalata\n\nDJ
+ OMEK (Blok 12)\nTalentovaný DJ\, bonviván\,
+ umělec\, pan kavárník známé zlínské
+ kavárny Blok 12 a milovník vyhlášených
+ balkánskych diskoték na Valašsku a
+ okolí.\n\nwww.blok12.cz\n\n●●●●●●●●●●●●●●●●●●●●●●●●●\n\nEnglish
+ description:\n\nWe are coming back to Brno with
+ our crazy balkan wedding! Be prepared for balkan
+ and gypsy madness\, columbian cumbia and french
+ electro swing. Only Balkan Bashavel this
+ summer!\n\n●●●●●●●●●●●●●●●●●●●●●●●●●\n\nFLÉDA
+ (malá scéna) Štefánikova 95/24\, Brno
+ \n23.8.2013 | 21:00\nVstup pro prvních dvacet
+ 70\,-\, potom 90\,-.\n\nZa podporu
+ děkujeme:\n\nFLÉDA: www.fleda.cz\n\n\nGo to
+ event:\nhttp://www.facebook.com/events/347850302011944
+CLASS:PUBLIC
+STATUS:CONFIRMED
+PARTSTAT:ACCEPTED
+END:VEVENT
+BEGIN:VEVENT
+DTSTAMP:20130819T075228Z
+LAST-MODIFIED:20130819T075228Z
+CREATED:20130819T075228Z
+SEQUENCE:0
+ORGANIZER;CN=Videoflot:MAILTO:noreply@facebookmail.com
+DTSTART;VALUE=DATE-TIME:20130822T180000Z
+DTEND;VALUE=DATE-TIME:20130822T210000Z
+UID:e1390579444502368@facebook.com
+SUMMARY:Startup Summer: Episode III - Piráti ze Silicon
+ Valley
+LOCATION:Hospůdka Sanghai\, Trtílkova 24\, 61200 Brno
+URL:http://www.facebook.com/events/1390579444502368/
+DESCRIPTION:Už jste viděli nový film o Stevu Jobsovi?
+ Přijďte si připomenout jednu starou klasiku a
+ srovnat... \n\nGo to
+ event:\nhttp://www.facebook.com/events/1390579444502368
+CLASS:PUBLIC
+STATUS:CONFIRMED
+PARTSTAT:ACCEPTED
+END:VEVENT
+END:VCALENDAR
