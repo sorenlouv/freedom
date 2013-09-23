98eda3ca020225cb5efca1275dd856452edb2243
Søren Louv-Jansen <sorenlouv@gmail.com>
2013-09-24 00:14:55 +0200

    Added pagoda config files

---
+++ pagoda/facebook.php
@@ -0,0 +1,6 @@
+<?php
+ return array(
+  "appId" => $_SERVER["CLIENT_ID"],
+  "secret" => $_SERVER["CLIENT_SECRET"],
+  "appAccessToken" => $_SERVER["APP_ACCESS_TOKEN"]
+);
---
+++ pagoda/freedom.php
@@ -0,0 +1,4 @@
+<?php
+ return array(
+  "salt" => $_SERVER["SALT"]
+);
