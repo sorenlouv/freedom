90f7d3652694b6b658e63ef99a2f85c49c229061
Søren Louv-Jansen <slo@tradeshift.com>
2014-04-12 15:03:26 -0700

    Fix for legacy handler

--- public/handlers.php
+++ public/handlers.php
@@ -2,9 +2,9 @@
 
 $function = isset($_GET['f']) ? $_GET['f'] : null;
 $user_id = isset($_GET['user_id']) ? $_GET['user_id'] : null;
-$secure_hash = isset($_GET['user_id']) ? $_GET['user_id'] : null;
+$secure_hash = isset($_GET['secure_hash']) ? $_GET['secure_hash'] : null;
 
 if($function === "downloadFeed"){
-  header("Location: /feeds/download-feed");
+  header("Location: /feeds/download-feed?user_id=" . $user_id ."&secure_hash=".$secure_hash);
 }
 ?>
