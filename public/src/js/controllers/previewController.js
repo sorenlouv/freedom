5d9d102f296b4f7e6d7461180820e7dfeae8f89b
Søren Louv-Jansen <slo@tradeshift.com>
2014-04-12 04:08:05 -0700

    Removed obsolete preview files

--- public/src/js/controllers/previewController.js
+++
@@ -1,9 +0,0 @@
-freedomApp.controller('previewController', function ($scope, $http) {
-  'use strict';
-  $scope.isLoading = true;
-
-  $http.get('/feeds/preview/').success(function(data, status) {
-    $scope.events = data;
-    $scope.isLoading = false;
-  });
-});
